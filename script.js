898f4eb513510ec35cbab3eeeaa53a987985c7e0
Dinesh Priyantha <dineshpriyantha200248@gmail.com>
2024-06-16 15:19:24 +0530

    update banner section#

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+(function() {
+    let slideIndex = 1;
+    showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}    
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+    }
+
+    setInterval(() => {
+        plusSlides(1);
+    }, 5000); // Change image every 5 seconds
+
+    // Attach event listeners to dots for manual navigation
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
+    for (let j = 0; j < dots.length; j++) {
+        dots[j].addEventListener("click", function() {
+            currentSlide(j + 1);
+        });
+    }
+})();
+
+
+let slideIndex = 2;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides-2343");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
     slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
-
-setInterval(() => {
-    plusSlides(1);
-}, 5000); // Change image every 5 seconds
